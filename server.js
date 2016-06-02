8cb5ad60791d515ba67e3678015c4469b0e1b92f
Marie Godon <Marie@Maries-MacBook-Air.local>
2016-06-02 17:43:55 -0400

    something else

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var MEGATRONS = [];
 app.use(express.static('public'));
 
 app.get('*', function (req, res) {
-  res.sendFile(__dirname + 'public/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 
